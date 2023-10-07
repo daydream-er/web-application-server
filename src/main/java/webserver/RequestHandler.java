c8e1d7aa18ba210a776ef50c457fa1857503ffef
kim sangwon <levelmax14@gmail.com>
2023-10-07 21:03:37 +0900

    Get sign-up

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,12 +1,14 @@
 package webserver;
 
 import java.io.DataOutputStream;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +16,8 @@ import org.slf4j.LoggerFactory;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
+import model.User;
+import util.HttpRequestUtils;
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
@@ -35,20 +39,38 @@ public class RequestHandler extends Thread {
 
         	request_url = true;
         	String[]	tokens = null;
-        	while (!(line = br.readLine()).equals("") && !(line == null))
+        	while (true)
         	{
+        		line = br.readLine();
+        		if (line == "" || line == null)
+        			break ;
         		if (request_url)
         		{
         			tokens = line.split(" ");
         			request_url = false;
         		}
-        		System.out.println("sangwoki" + line);
+        		System.out.println("$>" + line);
         	}
         	// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
             // byte[] body = "Hello World".getBytes();
+            byte[] body;
         	String	url = tokens[1];
-            byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+        	if (url == "/index.html")
+        		body = Files.readAllBytes(new File("./webapp" + url).toPath());
+        	else
+        	{
+        		body = "Hello World".getBytes();
+        		if (url.indexOf('?') != -1)
+        		{
+        			String reqeustPath = url.substring(0, url.indexOf('?'));
+        			System.out.println(reqeustPath);
+        			String params = url.substring(url.indexOf('?'));
+        			Map<String, String> um = HttpRequestUtils.parseQueryString(params);
+        			User	user = new User(um.get("userId"), um.get("password"), um.get("name"), um.get("email"));
+        			System.out.println(user.toString());
+        		}
+        	}
         	response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
